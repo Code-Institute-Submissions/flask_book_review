aeb7ff6af8b67a7a29cfe98b1b1ec7e8a2be18f7
saraa123 <25009404+saraa123@users.noreply.github.com>
2020-02-23 11:54:39 +0000

    Deleted debug=True.

--- app.py
+++ app.py
@@ -480,5 +480,4 @@ def delete_book(book_id):
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+    port=int(os.environ.get('PORT')))
